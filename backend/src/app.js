853e643c9b6fd8599b001fe33e9c1e087f3785ee
Sahin Meric <sahin.meric@medisprout.com>
2024-10-08 21:32:24 -0500

    Integrated register route into app.js for user registration

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 const sequelize = require('./config/db');
 const User = require('./models/User');
+const authRoutes = require('./routes/authRoutes');
+
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use('/api/auth', authRoutes);
+
 
 sequelize.authenticate()
   .then(() => console.log('Database connected...'))
