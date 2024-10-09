97ce1ad799b6dc004f03cd6276ae6a203ce3ebdc
Sahin Meric <sahin.meric@medisprout.com>
2024-10-08 21:19:44 -0500

    added User model

---
+++ backend/src/models/User.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const User = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.ENUM('employee', 'admin'),
+    allowNull: false,
+    defaultValue: 'employee',
+  },
+});
+
+module.exports = User;
