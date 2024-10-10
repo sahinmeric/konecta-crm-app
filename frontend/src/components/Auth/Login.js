3d972c87a662756fdf5c698d772069f5923331a0
Sahin Meric <sahin.meric@medisprout.com>
2024-10-10 08:51:29 -0500

    Basic login created along with navigating depending on role

--- frontend/src/components/Auth/Login.js
+++ frontend/src/components/Auth/Login.js
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { loginUser, setAuthToken } from '../../services/api';
 
 function Login() {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await loginUser(formData);
+      const token = response.data.token;
+      console.log('User logged in successfully:', response.data);
+      alert('Login Successful!');
+
+      setAuthToken(token);
+
+      localStorage.setItem('token', token);
+
+      const userRole = response.data.role;
+      if (userRole === 'admin') {
+        navigate('/admin');
+      } else {
+        navigate('/employee');
+      }
+    } catch (error) {
+      console.error('Error logging in user:', error);
+      alert('Login Failed!');
+    }
+  };
+
   return (
     <div>
-      <h2>Login Page</h2>
-      <form>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
         <div>
-          <label>Username:</label>
-          <input type="text" name="username" />
+          <label>Username: </label>
+          <input type="text" name="username" value={formData.username} onChange={handleChange} required />
         </div>
         <div>
-          <label>Password:</label>
-          <input type="password" name="password" />
+          <label>Password: </label>
+          <input type="password" name="password" value={formData.password} onChange={handleChange} required />
         </div>
         <button type="submit">Login</button>
       </form>
