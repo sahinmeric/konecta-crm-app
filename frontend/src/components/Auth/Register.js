f89b365092c14efeee09c26824d4066dd13549ab
Sahin Meric <sahin.meric@medisprout.com>
2024-10-10 08:51:50 -0500

    simple register form and navitagion implemented

--- frontend/src/components/Auth/Register.js
+++ frontend/src/components/Auth/Register.js
@@ -1,21 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { registerUser } from '../../services/api';
 
 function Register() {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+    role: 'employee',
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await registerUser(formData);
+      console.log('User registered successfully:', response.data);
+      alert('Registration Successful!');
+      navigate('/login');
+    } catch (error) {
+      console.error('Error registering user:', error);
+      alert('Registration Failed!');
+    }
+  };
+
   return (
     <div>
-      <h2>Register Page</h2>
-      <form>
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
         <div>
-          <label>Username:</label>
-          <input type="text" name="username" />
+          <label>Username: </label>
+          <input type="text" name="username" value={formData.username} onChange={handleChange} required />
         </div>
         <div>
-          <label>Password:</label>
-          <input type="password" name="password" />
+          <label>Password: </label>
+          <input type="password" name="password" value={formData.password} onChange={handleChange} required />
         </div>
         <div>
-          <label>Role:</label>
-          <select name="role">
+          <label>Role: </label>
+          <select name="role" value={formData.role} onChange={handleChange}>
             <option value="employee">Employee</option>
             <option value="admin">Admin</option>
           </select>
