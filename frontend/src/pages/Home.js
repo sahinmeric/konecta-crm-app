b6daa5ac5c38e0b0f0130d08dcab165bac90e598
Sahin Meric <sahin.meric@medisprout.com>
2024-10-10 15:25:08 -0500

    home updated with mui

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button, Typography, Container, Grid2 } from '@mui/material';
 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to Konecta CRM App</h1>
-      <Link to="/login">Login</Link> | <Link to="/register">Register</Link>
-    </div>
+    <Container
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        backgroundColor: 'background.default',
+      }}
+    >
+      <Typography
+        variant="h3"
+        component="h1"
+        sx={{
+          marginBottom: 4,
+          textAlign: 'center',
+        }}
+      >
+        Welcome to Konecta CRM App
+      </Typography>
+      <Grid2 container spacing={2} justifyContent="center">
+        <Grid2 item>
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ margin: 2 }}
+            component={Link}
+            to="/login"
+          >
+            Login
+          </Button>
+        </Grid2>
+        <Grid2 item>
+          <Button
+            variant="outlined"
+            color="secondary"
+            sx={{ margin: 2 }}
+            component={Link}
+            to="/register"
+          >
+            Register
+          </Button>
+        </Grid2>
+      </Grid2>
+    </Container>
   );
 }
 
